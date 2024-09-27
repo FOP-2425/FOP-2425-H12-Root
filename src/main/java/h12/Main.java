341eed7b938a95577f950253e2bade894585af31
nyanyan <zentox@users.noreply.github.com>
2024-09-27 12:00:56 +0200

    Example for testing

--- src/main/java/h12/Main.java
+++ src/main/java/h12/Main.java
@@ -1,5 +1,16 @@
 package h12;
 
+import h12.io.encoding.Decoder;
+import h12.io.encoding.Encoder;
+import h12.io.encoding.HuffmanCodingDecoder;
+import h12.io.encoding.HuffmanCodingEncoder;
+import h12.util.Bytes;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
 /**
  * Main entry point in executing the program.
  */
@@ -10,7 +21,22 @@ public class Main {
      *
      * @param args program arguments, currently ignored
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
+
+        ByteArrayInputStream in = new ByteArrayInputStream("Hello World!".getBytes());
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        Encoder encoder = new HuffmanCodingEncoder(in, out);
+        encoder.encode();
+
+        System.out.println(Arrays.toString(out.toByteArray()));
+        System.out.println(Bytes.toBits(out.toByteArray()));
+
+        in = new ByteArrayInputStream(out.toByteArray());
+        out = new ByteArrayOutputStream();
+        Decoder decoder = new HuffmanCodingDecoder(in, out);
+        decoder.decode();
+        System.out.println(out);
     }
 }
