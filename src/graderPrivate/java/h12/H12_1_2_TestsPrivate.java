d7441715c774d9ff3be3cf783c077dc0bda14c36
nyanyan <zentox@users.noreply.github.com>
2024-12-02 18:36:58 +0100

    2/16 private tests done

--- src/graderPrivate/java/h12/H12_1_2_TestsPrivate.java
+++ src/graderPrivate/java/h12/H12_1_2_TestsPrivate.java
@@ -79,8 +79,6 @@ public class H12_1_2_TestsPrivate extends H12_Tests {
         // Validate output
         Assertions2.assertEquals(expectedPos, position.get(stream), context,
             comment -> "Position was not updated correctly.");
-        Assertions2.assertEquals(initialBuffer, new MyByte(out.toByteArray()[0]),
-            context, comment -> "Buffer was written correctly");
         Assertions2.assertEquals(expectedBuffer, buffer.get(stream), context,
             comment -> "Buffer was not updated correctly.");
     }
@@ -116,5 +114,40 @@ public class H12_1_2_TestsPrivate extends H12_Tests {
             comment -> "Position was not updated correctly.");
         Assertions2.assertEquals(initialBuffer, buffer.get(stream), context,
             comment -> "Buffer was not updated correctly.");
+    }
+
+    @DisplayName("Die Methode flushBuffer() schreibt das Zeichen in den internen OutputStream korrekt.")
+    @Test
+    void testFlushBufferWrite() throws Throwable {
+             // Access test method and fields
+        MethodLink flushBuffer = getMethod("flushBuffer");
+        FieldLink position = Links.getField(getType(), "position");
+        FieldLink buffer = Links.getField(getType(), "buffer");
+
+        // Test data
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        stream = new BufferedBitOutputStream(out);
+        int initialPosition = 2;
+        position.set(stream, initialPosition);
+        MyByte initialBuffer = new MyByte(69);
+        buffer.set(stream, initialBuffer);
+
+        int expectedPos = MyByte.NUMBER_OF_BITS - 1;
+        MyByte expectedBuffer = new MyByte();
+
+        // Context information
+        Context context = contextBuilder(flushBuffer)
+            .add("Initial position", initialPosition)
+            .add("Initial buffer", initialBuffer)
+            .add("Expected position after flush", expectedPos)
+            .add("Expected buffer after flush", expectedBuffer)
+            .build();
+
+        // Test method
+        flushBuffer.invoke(stream);
+
+        // Validate output
+        Assertions2.assertEquals(initialBuffer, new MyByte(out.toByteArray()[0]),
+            context, comment -> "Buffer was written correctly");
     }
 }
